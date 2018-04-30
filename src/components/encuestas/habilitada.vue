259df1b37efcb9fdb5ef6431676f86e4de1bb3be
Ignacio Guri <ignacioguri@gmail.com>
2018-04-30 15:22:20 -0300

    Vestigios de Ver resultados

--- src/components/encuestas/habilitada.vue
+++ src/components/encuestas/habilitada.vue
@@ -1,9 +1,25 @@
 <template>
     <div>
-      <p>Encuesta para la semana #{{semana}}</p>
-      <div v-for="c in criterios" :key="c.idCriteriosXEncuesta">
-        <criterio-view :criterio="c" @voto="capturarVoto"></criterio-view>
-      </div>
+        <button type="button" class="btn btn-outline-info" @click="vista = 'resultados'">Ver resultados</button>
+        <button type="button" class="btn btn-outline-success" @click="vista = 'votacion'">Votar</button>
+        <div v-if="vista === 'votacion'">
+            <h4>Semana n° {{semana}}</h4>
+            <div v-for="c in criterios" :key="c.idCriteriosXEncuesta">
+                <criterio-view :criterio="c" @voto="capturarVoto"></criterio-view>
+            </div>
+        </div>
+        <div v-if="vista === 'resultados'">
+            <h4>Semana n° {{semana}}</h4>
+            <dl class="row">
+                <div v-for="c in criterios" :key="c.idCriteriosXEncuesta">
+                <dt class="col-sm-3">{{c.idCriteriosXEncuesta}}</dt>
+                <dd class="col-sm-9">
+                    <p>{{c.criterio}}</p>
+                    <p>Votos: {{c.votos.length}}</p>
+                </dd>
+                </div>
+            </dl>
+        </div>
     </div>
 </template>
 <script>
@@ -24,7 +40,8 @@ import criterioView from './criterioFinal'
       },
       data () {
         return {
-          criterios: null
+          criterios: null,
+          vista: null
         }
       },
       mounted () {
